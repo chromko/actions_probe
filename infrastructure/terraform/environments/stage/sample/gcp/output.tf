f92f2c4375ef1b0f93febada66c97531025deb80
chromko <d.mishchenko@qlean.ru>
2019-09-16 08:23:17 +0300

    check new ci

--- infrastructure/terraform/environments/stage/sample/gcp/output.tf
+++ infrastructure/terraform/environments/stage/sample/gcp/output.tf
@@ -5,3 +5,7 @@ output instances_db_addr {
 output instances_app_addr {
   value = google_compute_instance.gcp-test[*].network_interface.0.access_config.0.nat_ip
 }
+
+output lb_addr {
+  value = google_compute_address.env-public.address
+}
